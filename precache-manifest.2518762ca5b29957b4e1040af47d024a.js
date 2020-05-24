bf183fc4eb7922bc077ac8f5f21f954e319002e1
michael <michael.anthony.allan@gmail.com>
2020-05-24 12:06:10 +0100

    Updates

---
+++ precache-manifest.2518762ca5b29957b4e1040af47d024a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8e29a204443ef5eb90e2cf11087dcd",
+    "url": "/gaudete_friend_viewer/index.html"
+  },
+  {
+    "revision": "1f44f4707d7806f4adfc",
+    "url": "/gaudete_friend_viewer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "710f40b38a6f8be6dc4f",
+    "url": "/gaudete_friend_viewer/static/js/2.36f78e47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gaudete_friend_viewer/static/js/2.36f78e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f44f4707d7806f4adfc",
+    "url": "/gaudete_friend_viewer/static/js/main.6605ed9d.chunk.js"
+  },
+  {
+    "revision": "573a8271558fec6242dd",
+    "url": "/gaudete_friend_viewer/static/js/runtime-main.a5e9301d.js"
+  }
+]);
